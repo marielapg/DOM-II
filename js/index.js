b472502f180c3efea5e2f7ec836df606e97cba82
marielapg <mpgonzales77@gmail.com>
2019-10-31 09:58:50 -0700

    updated the index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+let link = document.querySelectorAll(".nav .nav-link");
+
+ link.forEach(element => {
+     element.addEventListener('click', (event) => {
+         element.style.backgroundColor = 'blue';
+         element.style.color = 'white';
+         event.preventDefault();
+         Me.to(element, 3, {y:100, rotation: 90});
+
+     })
+ })
